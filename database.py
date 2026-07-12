0c5be1a080f107779a0ac63dad7ee1242e333b9e
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 04:58:15 +0330

    Rename hetzner_accounts table to accounts

--- database.py
+++ database.py
@@ -3,12 +3,21 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('database.sqlite')
     cursor = conn.cursor()
-    # ساخت جدول اکانت‌های هتزنر
+    
+    # جدول اکانت‌های هتزنر
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS hetzner_accounts (
+        CREATE TABLE IF NOT EXISTS accounts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             token TEXT UNIQUE
+        )
+    ''')
+    
+    # جدول کاربران و کیف پول
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            balance INTEGER DEFAULT 0
         )
     ''')
     conn.commit()
@@ -18,17 +27,17 @@ def add_hetzner_account(name, token):
     try:
         conn = sqlite3.connect('database.sqlite')
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO hetzner_accounts (name, token) VALUES (?, ?)', (name, token))
+        cursor.execute('INSERT INTO accounts (name, token) VALUES (?, ?)', (name, token))
         conn.commit()
         conn.close()
         return True
     except sqlite3.IntegrityError:
-        return False  # توکن تکراری است
+        return False
 
 def get_all_accounts():
     conn = sqlite3.connect('database.sqlite')
     cursor = conn.cursor()
-    cursor.execute('SELECT id, name, token FROM hetzner_accounts')
+    cursor.execute('SELECT id, name, token FROM accounts')
     accounts = cursor.fetchall()
     conn.close()
     return accounts
