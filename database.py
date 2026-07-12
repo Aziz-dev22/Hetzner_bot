e2a4a6ba9c5cbe7549cfb4f1c043ddba709aa5bf
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 06:10:24 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,29 +1,9 @@
 import sqlite3
-from datetime import datetime, timedelta
-
 def init_db():
     conn = sqlite3.connect('database.sqlite')
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, token TEXT UNIQUE)')
     c.execute('CREATE TABLE IF NOT EXISTS plans (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, cpu TEXT, ram TEXT, price REAL)')
+    # ذخیره سرورهای کاربران با تاریخ انقضا
     c.execute('CREATE TABLE IF NOT EXISTS user_servers (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, hetzner_id INTEGER, expire_date TIMESTAMP)')
-    conn.commit()
-    conn.close()
-
-def add_hetzner_account(name, token):
-    try:
-        conn = sqlite3.connect('database.sqlite')
-        c = conn.cursor()
-        c.execute('INSERT INTO accounts (name, token) VALUES (?, ?)', (name, token))
-        conn.commit()
-        conn.close()
-        return True
-    except: return False
-
-def get_all_accounts():
-    conn = sqlite3.connect('database.sqlite')
-    c = conn.cursor()
-    c.execute('SELECT * FROM accounts')
-    data = c.fetchall()
-    conn.close()
-    return data
+    conn.commit(); conn.close()
