d9fbc5a81e549932cca6d1bbb69056027fefa721
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 04:28:30 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import threading
+from bot import bot
+from web_panel import app
+
+def run_bot():
+    print("🤖 ربات تلگرام در حال اجراست...")
+    bot.polling(none_stop=True)
+
+def run_web():
+    print("🌐 پنل وب در حال اجراست...")
+    app.run(host='0.0.0.0', port=5000, use_reloader=False)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=run_bot)
+    t2 = threading.Thread(target=run_web)
+    
+    t1.start()
+    t2.start()
+    
+    t1.join()
+    t2.join()
