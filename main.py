6a1ffcf3c4bac9600466c19fb0ee977fb4ebad4f
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 08:15:58 +0330

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import threading, bot, web_panel
-from database import init_db
-
-if __name__ == '__main__':
-    init_db()
-    # اجرای همزمان ربات و پنل
-    threading.Thread(target=lambda: bot.bot.polling(none_stop=True), daemon=True).start()
-    web_panel.app.run(host='0.0.0.0', port=5000)
