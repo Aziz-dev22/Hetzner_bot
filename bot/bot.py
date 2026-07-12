f85dae4f81150ff223d359ebdc9950eefe701394
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 23:05:42 +0330

    Implement start command handler
    Added a start command handler to the bot.

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,13 @@
 from aiogram import Bot, Dispatcher, executor
 from config.settings import settings
-import logging
 
-logging.basicConfig(level=logging.INFO)
 bot = Bot(token=settings.BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# این ایمپورت‌ها باعث می‌شوند ربات تمام دکمه‌ها را شناسایی کند
-from bot.handlers import user_handlers
-from bot.handlers import wallet_handlers
-from bot.handlers import referral_handlers
+# مثال برای تست ربات
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.reply("ربات با موفقیت فعال شد!")
 
-if __name__ == '__main__':
+def run_bot():
     executor.start_polling(dp, skip_updates=True)
