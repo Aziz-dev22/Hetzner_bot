059d96e24b4ba21a4c54baa35664a84b2e3d465f
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 05:54:01 +0330

    Refactor bot and web panel execution in main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-import threading
-from bot import bot
-from web_panel import app
+import threading, bot, web_panel
 from database import init_db
 
-def run_bot():
-    print("🤖 ربات تلگرام در حال اجراست...")
-    bot.polling(none_stop=True)
-
-def run_web():
-    print("🌐 پنل وب روی پورت 5000 در حال اجراست...")
-    # دسترسی از همه آی‌پی‌ها باز است
-    app.run(host='0.0.0.0', port=5000, use_reloader=False)
-
 if __name__ == '__main__':
-    # ساخت دیتابیس و جداول در اولین اجرا
     init_db()
-    
-    t1 = threading.Thread(target=run_bot)
-    t2 = threading.Thread(target=run_web)
-    
-    t1.start()
-    t2.start()
-    
-    t1.join()
-    t2.join()
+    # اجرای ربات در یک ترد جداگانه
+    threading.Thread(target=bot.bot.polling, kwargs={'none_stop': True}, daemon=True).start()
+    # اجرای پنل وب
+    web_panel.app.run(host='0.0.0.0', port=5000)
