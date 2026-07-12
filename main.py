2ebe08222eaf421d56f5a3994b12c4f438a6402f
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-12 06:13:14 +0330

    Update bot polling to use lambda in threading
    Refactor threading for bot polling to use a lambda function.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from database import init_db
 
 if __name__ == '__main__':
     init_db()
-    # اجرای ربات در یک ترد جداگانه
-    threading.Thread(target=bot.bot.polling, kwargs={'none_stop': True}, daemon=True).start()
-    # اجرای پنل وب
+    # اجرای همزمان ربات و وب
+    threading.Thread(target=lambda: bot.bot.polling(none_stop=True), daemon=True).start()
     web_panel.app.run(host='0.0.0.0', port=5000)
